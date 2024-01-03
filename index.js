315fb8403d30b83ff479f8a6cd03eeaadc087a31
Trung Em ^_^ <tem2k2.dev@gmail.com>
2024-01-03 09:36:08 +0700

    Install Nodemon & inspector

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Trung Em'));
+app.get('/', (req, res) => {
+    const a = 2;
+    const b = 3;
+    const c = a + b;
+    return res.send('Hello Trung Em ^_^');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
