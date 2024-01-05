146c792f4cfa1eb0e4549fe621022cfffbcac355
Trung Em ^_^ <tem2k2.dev@gmail.com>
2024-01-06 01:00:01 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;
 const path = require('path');
+
+const route = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -26,19 +29,7 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.render('search');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
